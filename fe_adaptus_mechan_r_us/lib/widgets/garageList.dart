398612a3bb9778e136c3b524a2bad1a80e199fd1
jashdow <jamessashdown@gmail.com>
2019-10-23 16:01:33 +0100

    added InkWell wrapper and onTap functionality to the garageListCards

--- fe_adaptus_mechan_r_us/lib/widgets/garageList.dart
+++ fe_adaptus_mechan_r_us/lib/widgets/garageList.dart
@@ -7,7 +7,7 @@ class GarageList extends StatefulWidget {
 }
 
 class _GarageListState extends State<GarageList> {
-  List garages = [{"name": "garagetown", "price": "£££", "distance": "10miles"},{"name": "mechanic man", "price": "£", "distance": "12miles"},{"name": "bob bobsworth", "price": "££", "distance": "15miles"}];
+  List garages = [{"name": "garagetown", "price": "£££", "distance": "10miles"},{"name": "mechanic man", "price": "£", "distance": "12miles"},{"name": "bob bobsworth", "price": "££", "distance": "15miles"}, {"name": "the spannerworks", "price": "£££", "distance": "22miles"}, {"name": "johnny motors", "price": "££", "distance": "30miles"}];
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +23,26 @@ class _GarageListState extends State<GarageList> {
   Widget garageListCard (BuildContext context, int index) {
     return new Container(
       child: Card(
+        child: new InkWell(
+          onTap: () {
+            // can add navigation here to garage page
+            print(index.toString());
+          },
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Row(
             children: <Widget>[
-              Text(garages[index]["name"]),
+              Text(garages[index]["name"], style: new TextStyle(fontSize: 20.0),),
               Spacer(),
               Padding(
                 padding: const EdgeInsets.only(left: 16.0, right: 16.0),
                 child: Text(garages[index]["price"]),
               ),
-              Text(garages[index]["distance"])
+              Text(garages[index]["distance"]),
             ],
           ),
         ),
+      ),
       ),
     );
   }
