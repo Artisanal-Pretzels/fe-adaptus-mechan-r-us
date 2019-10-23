09d70330da512601eeee6452f6028a50876ff04e
jashdow <46503488+jashdow@users.noreply.github.com>
2019-10-23 10:30:40 +0100

    setup done screens and widgets folders created (#1)

---
+++ fe_adaptus_mechan_r_us/lib/screens/authentication/user-authentication.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserAuthenticate extends StatefulWidget {
+  @override
+  _UserAuthenticateState createState() => _UserAuthenticateState();
+}
+
+class _UserAuthenticateState extends State<UserAuthenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('user authenticate')
+          // login and registration widgets will go here
+    );
+  }
+}
---
+++ fe_adaptus_mechan_r_us/lib/screens/home/userHome.dart
@@ -0,0 +1,11 @@
+import  'package:flutter/material.dart';
+
+class UserHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('user home')
+          // List with garages and navbar will go here
+    );
+  }
+}
