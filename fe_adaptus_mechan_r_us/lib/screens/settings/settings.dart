3cdaeee380420389dd742d9edb4e3489f227f5c6
jashdow <jamessashdown@gmail.com>
2019-10-28 12:18:35 +0000

    added basic settings page

--- fe_adaptus_mechan_r_us/lib/screens/settings/settings.dart
+++ fe_adaptus_mechan_r_us/lib/screens/settings/settings.dart
@@ -7,9 +7,35 @@ class Settings extends StatefulWidget {
 
 class _SettingsState extends State<Settings> {
   @override
+
+
+
   Widget build(BuildContext context) {
     return Container(
-      child: Text('settings motherfucker!')
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+            Row(
+              children: [Text('Username:'), Text('gazboybaldwin69')],
+            ),
+              SizedBox(height: 50),
+            Row(
+            children: [Text('Name:'), Text('Gareth Baldwin')],
+            ),
+              SizedBox(height: 50),
+            Row(
+              children: [Text('Email:'), Text('gazzabazzabaldo91@gmail.com')],
+            ),
+              SizedBox(height: 50),
+            Row(
+              children: [Text('Phone Number:'), Text('07894561230')],
+            ),
+            ]
+        ),
+      ),
     );
   }
 }
