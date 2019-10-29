ef5bf4891d4e713868fee198ab8c0e88d69f44d4
jashdow <jamessashdown@gmail.com>
2019-10-29 12:52:41 +0000

    api request working and garageList being populated with Garage objects

--- fe_adaptus_mechan_r_us/lib/classes/garage.dart
+++ fe_adaptus_mechan_r_us/lib/classes/garage.dart
@@ -1,7 +1,24 @@
 class Garage {
-  final String name;
-  final String price;
-  final String distance;
+  int garageID;
+  String garageName;
+  double ratings;
+  double basePrice;
+  dynamic distance;
 
-  Garage(this.name, this.price, this.distance);
+
+
+  Garage(this.garageID, this.garageName, this.ratings, this.basePrice, this.distance);
+
+  factory Garage.fromJson(Map<String, dynamic> json) {
+    return Garage(
+      json['garageID'] as int,
+      json['garageName'],
+      json['ratings'].toDouble(),
+      json['basePrice'].toDouble(),
+      json['distance']
+    );
+  }
 }
+
+
+//garageID: json['garageID'] as int, garageName: json['garageName'], ratings: json['ratings'] as double, basePrice: json['basePrice'] as double, distance: json['distance'] as Map
