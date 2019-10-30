9e23398e2555d585d972471b5092142bda4730c4
smetca <49376655+smetca@users.noreply.github.com>
2019-10-30 14:23:20 +0000

    Fix MaterialApp breaking (#16)
    * Remove wrapper
    
    * Add UserHome correctly to app

--- fe_adaptus_mechan_r_us/lib/main.dart
+++ fe_adaptus_mechan_r_us/lib/main.dart
@@ -12,7 +12,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return UserHome();
+
+    return MaterialApp(
+      home: UserHome(),
+    );
+
   }
 }
 
