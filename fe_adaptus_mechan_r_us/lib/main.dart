0268a1be60f615063883155d8acb815df2a4e29f
smetca <s.metcalfe.h@gmail.com>
2019-10-30 19:14:49 +0000

    Add login routing

--- fe_adaptus_mechan_r_us/lib/main.dart
+++ fe_adaptus_mechan_r_us/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:fe_adaptus_mechan_r_us/src/screens/log_in/logIn.dart';
 import 'package:fe_adaptus_mechan_r_us/src/screens/user-wrapper.dart';
 import 'package:fe_adaptus_mechan_r_us/src/screens/home/userHome.dart';
 import 'package:fe_adaptus_mechan_r_us/src/classes/User.dart';
+import 'package:fe_adaptus_mechan_r_us/src/screens/garage_profile/garageProfile.dart';
+import 'package:fe_adaptus_mechan_r_us/src/api/api.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -22,14 +24,20 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    if (user != null) {
-      return MaterialApp(
-        home: UserHome(),
-      );
-    } else {
+     if (user == null) {
       return MaterialApp(
         home: new UserAuthenticate(
           getUser
+        ),
+      );
+    } else if (user?.garageID == null) {
+       return MaterialApp(
+         home: UserHome(),
+       );
+     } else {
+      return MaterialApp(
+        home: new GarageProfile(
+          user.garageID
         ),
       );
     }
