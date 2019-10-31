0e86640bcfe990084780586efb3a1273f8ec9dad
smetca <s.metcalfe.h@gmail.com>
2019-10-31 23:45:08 +0000

    Extract ReviewList into new file

--- fe_adaptus_mechan_r_us/lib/src/screens/garage_details/garageDetails.dart
+++ fe_adaptus_mechan_r_us/lib/src/screens/garage_details/garageDetails.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:fe_adaptus_mechan_r_us/src/api/api.dart';
 import 'package:fe_adaptus_mechan_r_us/src/classes/Review.dart';
-
+import 'package:fe_adaptus_mechan_r_us/src/screens/garage_details/ReviewList.dart';
 import '../video_call/Signaling.dart';
 
 //class  extends StatelessWidget {
@@ -272,64 +272,4 @@ class TopImage extends StatelessWidget {
 //
 //  return fetchedGarage;
 //}
-
-class ReviewsList extends StatefulWidget {
-  final List<dynamic> reviews;
-
-  ReviewsList(this.reviews);
-
-  @override
-  _ReviewsListState createState() => _ReviewsListState();
-}
-
-class _ReviewsListState extends State<ReviewsList> {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300.0,
-        child: Container(child: ListView.builder(
-          itemCount: widget.reviews.length,
-        itemBuilder: (BuildContext context, int index) => reviewsListCard(context, index)))
-    );}
-
-  Widget  reviewsListCard(BuildContext context, int index) {
-    return new Container (
-      child: Card(
-          child: Column(
-            children: [
-              Text(
-                widget.reviews[index].body,
-                style: new TextStyle(fontSize: 20.0),
-              ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Row (
-
-                        children: [Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: Text('Username: ${widget.reviews[index].username}'),
-                      ),
-
-//
-//                        padding: const EdgeInsets.only(left: 16.0, right: 70.0),
-                        Text('Rating: ${widget.reviews[index].rating.toString()}'),
-                        ]
-                      )
-                    )
-                  ]
-                ),
-
-
-            ]
-          )
-
-          ),
-      );
-
-  }
-
-}
 
